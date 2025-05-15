46ac770a1086f5301a66a547a6e73b333ab7fa20
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2025-05-15 15:50:30 -0300

    Update criarClasse2.php

--- BancoDados/criarClasse2.php
+++ BancoDados/criarClasse2.php
@@ -5,4 +5,23 @@ require_once "conexao.php";
 class criarClasse2{
     private $tbBanco = "Tables_in_enderecos";
     private $con;
+
+
+    function classesModel(){
+        $sql = "SHOW TABLES";
+        $query = $this->con->query($sql);
+        $tabelas = $query->fetchAll(PDO::FETCH_OBJ);
+
+        foreach($tabelas as $tabela){
+            $nomeTabela = ucfirst(($tabela->{$this->tbBanco}));
+            $conteudo = <<<EOT
+
+class ($nomeTabela){
 }
+EOT;
+
+            echo "conteudo:<br><pre>$conteudo</pre><br><br>"
+        }
+    }
+}
+
